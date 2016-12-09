f0f8478d36901ba68a566c2264d031c3462221ad
Alexey Bille <alexey.bille@mail.ru>
2016-12-09 20:14:30 +0700

    fix styles

---
+++ axit/js/script.js
@@ -0,0 +1,10 @@
+$('.parallax-window').parallax({
+    imageSrc: 'img/u25_state0.png'
+});
+
+$(document).ready(function (ev) {
+    $('#custom_carousel').on('slide.bs.carousel', function (evt) {
+        $('#custom_carousel .controls li.active').removeClass('active');
+        $('#custom_carousel .controls li:eq(' + $(evt.relatedTarget).index() + ')').addClass('active');
+    })
+});
