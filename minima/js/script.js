17a97ccd74b5ac2b1d24f5ad8e2e3c7491458bb1
Alexey Bille <alexey.bille@mail.ru>
2016-12-09 20:10:25 +0700

    new site

--- minima/js/script.js
+++
@@ -1,9 +0,0 @@
-var speed=300,
-    originalHeight = 0,
-    hoverHeight=60;
-
-$('.brand-item').hover(function(){
-    $(this).find('.hidden-element').stop().animate({width:$(this).css('width')},0).animate({height:hoverHeight},speed);
-},function(){
-    $(this).find('.hidden-element').stop().animate({width:$(this).css('width')},0).animate({height:originalHeight},speed);
-})
