511bab1f157a00e35f34b1a1a7c869ad060b292a
Alexey Bille <alexey.bille@gmail.com>
2017-02-25 15:33:45 +0700

    see in CV

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-	$(".nav").on("click","a", function (event) {
+	$(".navbar").on("click","a", function (event) {
 		//отменяем стандартную обработку нажатия по ссылке
 		event.preventDefault();
 
@@ -10,21 +10,62 @@ $(document).ready(function(){
 		//узнаем высоту от начала страницы до блока на который ссылается якорь
 			top = $(id).offset().top - 50;
 		
-		//анимируем переход на расстояние - top за 1500 мс
+		//анимируем переход на расстояние - top за x мс
 		$('body,html').animate({scrollTop: top}, 900);
 	});
+
     $(".fa-question-circle-o").hover(function(){
         $(this).after("<div class='quest-hover'>"+$(this).attr("info")+"<div>");
-        var position = $(this).position();
+        var blockLeftPosition = $(this).position().left;
+        var blockTopPosition = $(this).position().top;
+        var offsetLeftPosition = $(this).offset().left;
+        var docWidth = $(document).width();
+        var blockWidth = $(this).next().width();
+        var leftPosition = 0;
+        var shift = 40;
+        if(offsetLeftPosition + 46 + blockWidth < docWidth ){
+                leftPosition = blockLeftPosition + 20;
+        }else {
+            leftPosition = 0 ;
+            blockTopPosition += 30;
+            shift = -40;
+        }
+        console.log("LeftPosition = " + leftPosition);
+        console.log("docWidth = " + docWidth);
+        console.log("offsetLeftPosition = " + offsetLeftPosition);
+        console.log("blockWidth = " + blockWidth);
         $(this).next().css({"display":"inline",
-                           "left":position.left + 20,
-                          "top":position.top - 40}).animate({
-            top:position.top
+                           "left":leftPosition,
+                          "top":blockTopPosition - shift}).animate({
+            top:blockTopPosition
         },200,function(){});
         
     }, function(){
-        $(this).next().remove();
+        $(this).next().fadeOut(200).delay(200).queue(function() { $(this).remove(); });;
+
     });
+
+    var s1 = $('#section-1').offset().top + 300;
+    var s2 = $('#section-2').offset().top - 300;
+    var s3 = $('#section-3').offset().top - 300;
+    var footer = $('#footer').offset().top - 500;
+
+
+    function toggleActiveClass(a) {
+        removeActiveClass();
+        $(".navbar-nav>li:nth-child(" + a + ")").find('a.go_to').addClass('active')
+    }
+    function removeActiveClass() {
+        $('.go_to.active').removeClass('active');
+    }
+    $(window).scroll(function () {
+        var bo = $("body").scrollTop();
+        if (bo > s2) toggleActiveClass(1);
+        if (bo > s3) toggleActiveClass(2);
+        if (bo > footer) toggleActiveClass(3);
+        if (bo < s1) removeActiveClass();
+    })
+
 });
 
 
